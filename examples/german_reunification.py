b4655902ad8147f5b2b2cb78dfd5ece4933e4836
OscarEngelbrektson <35859702+OscarEngelbrektson@users.noreply.github.com>
2020-12-05 11:24:35 +0100

    Updated german_reunification example

--- examples/german_reunification.py
+++ examples/german_reunification.py
@@ -1,7 +1,7 @@
 #Import packages
 import pandas as pd
 import numpy as np
-from synth import Synth
+from SyntheticControlMethods import Synth
 
 #Import data
 data = pd.read_csv("examples/datasets/german_reunification.csv")
@@ -9,24 +9,22 @@ data = data.drop(columns="code", axis=1)
 
 #Fit Synthetic Control
 synth = Synth(data, "gdp", "country", "year", 1990, "West Germany")
-synth.in_time_placebo(1982)
-synth.in_space_placebo()
 
-synth.plot(['in-space placebo', 'in-time placebo'], in_space_exclusion_multiple=5, treated_label="West Germany",
-            synth_label="Synthetic West Germany")
-synth.plot(['in-time placebo'], in_space_exclusion_multiple=5, treated_label="West Germany",
-            synth_label="Synthetic West Germany")
-
+'''
 synth.plot(["original", "pointwise", "cumulative"], treated_label="West Germany", 
             synth_label="Synthetic West Germany", treatment_label="German Reunification")
-
+'''
 
 #Perform validity tests
-synth.in_time_placebo(1986) #Placebo treatment period is 1986, 4 years earlier
+synth.in_time_placebo(1982) #Placebo treatment period is 1982, 8 years earlier
 synth.in_space_placebo()
 
-#Visualize validity tests
-synth.plot(['in-space placebo', 'pre/post rmspe', 'in-time placebo'], in_space_exclusion_multiple=5, treated_label="West Germany",
+synth.plot(['in-space placebo', 'in-time placebo'], in_space_exclusion_multiple=5, 
+            treated_label="West Germany",
+            synth_label="Synthetic West Germany")
+
+synth.plot(['pre/post rmspe'], 
+            treated_label="West Germany",
             synth_label="Synthetic West Germany")
 
 '''
