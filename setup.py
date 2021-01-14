9a52facf490759ce7d14ae0f99b2c00046a9a71d
OscarEngelbrektson <35859702+OscarEngelbrektson@users.noreply.github.com>
2021-01-14 14:52:22 -0800

    Changed cvxpy version requirement

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
         'numpy',
         'scipy==1.4.1',
         'pandas',
-        'cvxpy==1.1.4',
+        'cvxpy',
         'matplotlib>=2.2.3',
         'jinja2>=2.10'
     ]
