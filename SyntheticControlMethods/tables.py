9cdff92ae7e68b6a148dd8581ff3478562992097
OscarEngelbrektson <35859702+OscarEngelbrektson@users.noreply.github.com>
2021-03-17 17:12:59 -0700

    Improved tables.py
    * Improved documentation
    * Dataframes now only show two decimal digits for readability

--- SyntheticControlMethods/tables.py
+++ SyntheticControlMethods/tables.py
@@ -3,24 +3,20 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import pandas as pd
 
-class SummaryTables(object):
-    '''
-    rmspe_df
-    comparison_df
-    weight_df
-    '''
+class Tables(object):
+    ''' This class constructs Pandas DataFrames that summarize important information about Synthetic Controls.'''
     def _get_weight_df(self, data):
       '''Prepares dataframe with weight assigned to each unit in synthetic control'''
       
-      weight_df = pd.DataFrame({"Unit":data.control_units,
-                                        "Weight":data.w.T[0]})
+      weight_df = pd.DataFrame({"Weight":data.w.T[0]},
+                                index=data.control_units)
       #Show only units with non-zero weights
-      return weight_df.loc[weight_df["Weight"] > 0.00001] 
+      return weight_df.loc[weight_df["Weight"] > 0.01] 
 
     
     def _get_comparison_df(self, data):
       '''
-      Returns dataframe with shape (n_covariates, 4).
+      Returns dataframe with shape (n_covariates, 5).
       The four columns are:
 
         self.original_data.treated_unit: 
@@ -31,17 +27,20 @@ class SummaryTables(object):
           Unscaled, covariate values of the synthetic control unit
           If method == DSC, then the differenced data is displayed instead
       
-        WMAUE:
-          Weighted Mean Absolute Unitwise Error. For each covariate, how different is each control 
+        WMAPE:
+          Weighted Mean Absolute Pairwise Error. For each covariate, how different is each control 
           unit inside the synthetic control from the treated unit, weighted by the weight assigned to each unit.
           This does not change even if method == DSC, as bias scales with value of difference and not change
-        
         
         Importance:
           Leading diagonal of V matrix. How important, relative to other covariates,
           is matching on each covariate in the optimization process?
           Note that this is computed after rescaling each covariate to be unit variance, 
           whereas the other columns show the unscaled covariate values.
+        
+        Control Group Average:
+          Simple average of all the units in the control group. Not strictly necessary for anything,
+          but it is often interesting to see how well the synthetic control is doing. 
       
       Interpretation:
       If the synthetic control has good fit, the following things should be true:
@@ -51,7 +50,7 @@ class SummaryTables(object):
 
         2. The third column should be small, relative to the values in columns 1 and 2. 
            The closer to zero, the more similar the individual control units inside the syntetic control are to the treated unit.
-           The smaller the MWAUE, the lower the potential bias, all else equal.
+           The smaller the WMAPE, the lower the potential bias, all else equal.
         
         3. There is no fixed way to interpret the importance column. Instead, it should be evaluated using domain knowledge.
            Is the relative importance assigned to each covariate reasonable given the context?
@@ -63,11 +62,11 @@ class SummaryTables(object):
       unscaled_control_covariates = self.original_data.unscaled_control_covariates
       wmape = (np.abs((unscaled_treated_covariates - unscaled_control_covariates)) @ data.w).reshape(data.n_covariates,)
       
-      comparison_df = pd.DataFrame({data.treated_unit: data.unscaled_treated_covariates.ravel(),
-                                    "Synthetic " + data.treated_unit: (data.unscaled_control_covariates @ data.w).ravel(),
-                                    "WMAPE": wmape,
-                                    "Importance":data.v,
-                                    "Control Group Average": data.unscaled_control_covariates.mean(axis=1)},
+      comparison_df = pd.DataFrame({data.treated_unit: np.round(data.unscaled_treated_covariates.ravel(), 2),
+                                    "Synthetic " + data.treated_unit: np.round((data.unscaled_control_covariates @ data.w).ravel(), 2),
+                                    "WMAPE": np.round(wmape, 2),
+                                    "Importance":np.round(data.v, 2)},
+                                    #"Control Group Average": np.round(data.unscaled_control_covariates.mean(axis=1), 2)},
                                     index=data.covariates)
       return comparison_df
 
@@ -146,6 +145,5 @@ class SummaryTables(object):
             pre_treatment = np.sqrt(((treated_outcome[:t] - synth_outcome[0][:t]) ** 2).mean())
     
             post_treatment = np.sqrt(((treated_outcome[t:] - synth_outcome[0][t:]) ** 2).mean())
-
 
         return post_treatment, pre_treatment
