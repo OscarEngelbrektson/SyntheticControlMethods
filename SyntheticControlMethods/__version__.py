3f496b36ed46c4e5c1e08ce6e903013e6eeb29df
Oscar Engelbrektson <35859702+OscarEngelbrektson@users.noreply.github.com>
2021-10-07 11:41:38 +0200

    Updated dependency requirements
    No longer strict dependency on any package, instead >= for all dependencies.

--- SyntheticControlMethods/__version__.py
+++ SyntheticControlMethods/__version__.py
@@ -10,6 +10,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION = (1, 1, 19)
+VERSION = (1, 1, 17)
 
 __version__ = '.'.join([str(e) for e in VERSION])
